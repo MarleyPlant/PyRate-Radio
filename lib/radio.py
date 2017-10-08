c6f50a2548606cbbba1772fee68560bce1848c2d
MarleyPlant <marley@marleyplant.com>
2017-10-08 19:21:57 +0100

    update subcall to correct for PiFM

--- lib/radio.py
+++ lib/radio.py
@@ -1,32 +1,31 @@
 import glob
 from subprocess import call
 import os
-songs = []
-songs_path = []
-
 
 class antenna:
 	def __init__(self, freq, stereo):
 		self.frequency = freq
 		self.stereo = stereo
+		self.songs = []
+		self.songs_path = []
+
 
 	def loadFiles(self):
 		os.chdir("music")
 		for file in glob.glob("*.wav"):
-			songs_path.append(os.path.realpath(file))
-			songs.append(file)
+			self.songs_path.append(os.path.realpath(file))
+			self.songs.append(file)
 		os.chdir("..")
 
 	def stop(self):
 		player.kill()
 
 	def play(self, selected):
-		print(str(songs_path[selected]))
-		player = os.system("./pifm " + str(songs_path[selected]) + " 103.3 22050 stereo")
+		player = os.system("./lib/pifm " + str(self.songs_path[selected]) + " 103.3 22050 stereo")
 
 	def list(self):
 		i = 0
-		if songs == []:
-			for item in songs:
+		if self.songs != []:
+			for item in self.songs:
 				i = i+1
 				print("[" + str(i-1) + "]" + item)
